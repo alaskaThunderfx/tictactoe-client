f84d9528400faa5eeabb893e4a52f65648d161e4
alaskaThunderfx <scott.thomas.mackey@gmail.com>
2021-05-13 15:11:57 -0400

    Improved auth ui

--- assets/scripts/authEvents/ui.js
+++ assets/scripts/authEvents/ui.js
@@ -22,13 +22,15 @@ const signUpSuccess = function (res) {
 
 const signInSuccess = function (res) {
   $('#sign-in').trigger('reset')
-  $('#messaging').html('<p>Sign in success!!!</p>')
+  $('#messaging').html(`
+    <p>Sign in success!!!</p>
+    <h1>Welcome back!</h1>
+    <h3>${res.user.email}</h3>
+    `)
   $('#messaging').addClass('success')
 
   console.log(res)
   $('#sign-in-display').html(`
-    <h1>Welcome back!</h1>
-    <h3>${res.user.email}</h3>
     `)
   $('#sign-in-display').addClass('success')
 
@@ -39,26 +41,24 @@ const signInSuccess = function (res) {
     $('#sign-in-display').removeClass('success')
   }, 5000)
 
+  $('.authentication').hide()
+
   store.user = res.user
 }
 
 const signOutSuccess = function () {
-  $('#messaging').html('<p>Sign out success!!!</p>')
-  $('#messaging').addClass('success')
-
-  console.log()
-  $('#sign-out-display').html(`
+  $('#messaging').html(`
+    <p>Sign out success!!!</p>
     <h1>Farewell!</h1>
-    <h3>${store.user.email}</h3>
-    `)
-  $('#sign-out-display').addClass('success')
-
+    <h3>${store.user.email}</h3>`)
+  $('#messaging').addClass('success')
+  console.log()
   setTimeout(() => {
     $('#messaging').html('')
-    $('#sign-out-display').html('')
     $('#messaging').removeClass('success')
-    $('#sign-out-display').removeClass('success')
   }, 5000)
+
+  $('.authentication').show()
 
   store.user = null
 }
