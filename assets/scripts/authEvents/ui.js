0827ac6e68fcafe1f794bfdc218bb31ddd9d5166
alaskaThunderfx <scott.thomas.mackey@gmail.com>
2021-05-13 15:33:04 -0400

    Further improved auth behavior

--- assets/scripts/authEvents/ui.js
+++ assets/scripts/authEvents/ui.js
@@ -2,28 +2,26 @@ const store = require('../store.js')
 
 const signUpSuccess = function (res) {
   $('#sign-up').trigger('reset')
-  $('#messaging').html('<p>Sign up success!!!</p>')
+  $('#messaging').html(`
+    <h1>Welcome! Thanks for joining us!</h1>
+    <h3>${res.user.email}</h3>
+    `)
   $('#messaging').addClass('success')
 
   console.log(res)
-  $('#sign-up-display').html(`
-    <h1>Welcome!</h1>
-    <h3>${res.user.email}</h3>
-    `)
-  $('#sign-up-display').addClass('success')
-
   setTimeout(() => {
     $('#messaging').html('')
-    $('#sign-up-display').html('')
     $('#messaging').removeClass('success')
-    $('#sign-up-display').removeClass('success')
   }, 5000)
+}
+
+const signUpFailure = function () {
+  $('#messaging').html('<p>Sign up failed...</p>')
 }
 
 const signInSuccess = function (res) {
   $('#sign-in').trigger('reset')
   $('#messaging').html(`
-    <p>Sign in success!!!</p>
     <h1>Welcome back!</h1>
     <h3>${res.user.email}</h3>
     `)
@@ -42,13 +40,17 @@ const signInSuccess = function (res) {
   }, 5000)
 
   $('.authentication').hide()
+  $('#hidden').removeClass('initial-hide')
 
   store.user = res.user
 }
 
+const signInFailure = function () {
+  $('#messaging').html('<p>Sign in failed...</p>')
+}
+
 const signOutSuccess = function () {
   $('#messaging').html(`
-    <p>Sign out success!!!</p>
     <h1>Farewell!</h1>
     <h3>${store.user.email}</h3>`)
   $('#messaging').addClass('success')
@@ -59,16 +61,9 @@ const signOutSuccess = function () {
   }, 5000)
 
   $('.authentication').show()
+  $('#hidden').addClass('initial-hide')
 
   store.user = null
-}
-
-const signUpFailure = function () {
-  $('#messaging').html('<p>Sign up failed...</p>')
-}
-
-const signInFailure = function () {
-  $('#messaging').html('<p>Sign in failed...</p>')
 }
 
 const signOutFailure = function () {
