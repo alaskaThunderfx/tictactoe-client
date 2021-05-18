616517be8a140f14edb08021a527c457a5d0d04f
alaskaThunderfx <scott.thomas.mackey@gmail.com>
2021-05-18 18:32:50 -0400

    Added some styling

--- assets/scripts/gameEvents/ui.js
+++ assets/scripts/gameEvents/ui.js
@@ -46,17 +46,17 @@ const indexGamesSuccess = function (res) {
 
 const gameEndXWonSuccess = function () {
   $('#victory').html('')
-  $('#victory').html('<h3>Puppy wins!</h3>')
+  $('#victory').html('<h3>Puppy wins!</h3><img src=https://media0.giphy.com/media/dudcZA9e14HIY/giphy.gif alt=happy class=puppy>')
 }
 
 const gameEndOWonSuccess = function () {
   $('#victory').html('')
-  $('#victory').html('<h3>Kitten wins!</h3>')
+  $('#victory').html('<h3>Kitten wins!</h3><img src=https://i0.wp.com/dianaurban.com/wp-content/uploads/2017/07/19-cute-kitten.gif?resize=330%2C328&ssl=1 alt=happy class=kitten>')
 }
 
 const gameEndTieSuccess = function () {
   $('#victory').html('')
-  $('#victory').html("<h3>It's a tie!</h3>")
+  $('#victory').html('<h3>It\'s a tie!</h3><img src=https://media1.giphy.com/media/XcabqzOLg2tz2/giphy.gif alt=happy class=tie>')
 }
 
 const cell0ClickSuccess = function () {
